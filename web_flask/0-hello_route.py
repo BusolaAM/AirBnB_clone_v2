1f5249b669492f68cfb0b0d0a46a536593bc8677
BusolaAM <busolaatinsola@gmail.com>
2023-05-18 12:23:11 +0100

    AirBnB clone v2

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,10 +10,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/airbnb-onepage/')
-def hello_route():
-    return "Hello, Route!"
-
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
